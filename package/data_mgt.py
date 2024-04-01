44f12cebd0c390691b3cb8f1b1ae893e5e8c8c73
ZewiL08 <will.moiroud@hotmail.fr>
2024-04-01 15:31:14 +0900

    Added new datasets

--- package/data_mgt.py
+++ package/data_mgt.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import darts.datasets
 
 
 def get_data_set(name, input_length, start_date, end_date) :
@@ -43,3 +44,50 @@ def get_data_set_return(name, input_length, start_date, end_date) :
         df_raw['return'] = ((df_raw['close'] / df_raw['open']) - 1) * 10000
         
     return (df_raw, train, test)
+
+
+def get_data_set_simple(input_length) :
+    with open('datasets/IstanbulTraffic.csv', 'rb') as f:
+        df = pd.read_csv(f,  parse_dates=[0], names=['date', 'close'])
+        df = df.head(input_length + 7)
+        print(df)
+        df['close'] = df['close'].astype(float)
+        series = pd.Series(df['close'].values, index=df['date'])
+
+        f.seek(0)
+        df_raw = pd.read_csv(f, parse_dates=[0], names=['date', 'close'])
+        df_raw = df_raw.head(input_length + 7)
+
+    train = series.iloc[:input_length]
+    test = series.iloc[input_length:]  
+
+    return (df_raw, train, test)
+
+
+def get_data_darts(input_length) :
+    dsname = "AirPassengersDataset"
+    darts_ds = getattr(darts.datasets,dsname)().load()
+    
+    print(darts_ds) 
+
+    df =   darts_ds.pd_dataframe()
+    df = df.head(input_length + 7)
+    df.columns = ['date', 'close']
+
+    print(df)
+
+    df['close'] = df['close'].astype(float)
+    series = pd.Series(df['close'].values, index=df['date'])
+
+    df_raw =   darts_ds.pd_dataframe()
+    df_raw = df.head(input_length + 7)
+    df_raw.columns = ['date', 'close']
+
+    train = series.iloc[:input_length]
+    test = series.iloc[input_length:]  
+
+    series = darts_ds.pd_series()
+    series = series.rename(columns={'time': 'date', 'value': 'close'})
+
+    return (df_raw, train, test)
+
--- package/plot.py
+++ package/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_preds_candles(df, train, test, pred, model_name, show_samples=False, savefig = ""):
+def plot_preds_candles(df, train, pred, model_name, savefig = ""):
 
     save_folder = "./plot/" + savefig
     index = len(train)
@@ -41,6 +41,43 @@ def plot_preds_candles(df, train, test, pred, model_name, show_samples=False, sa
     index_to_display = range(0, len(truth), len(truth) // 3)
     truth.index = truth.index.date
     display_date = truth.index[index_to_display]
+    plt.xticks(index_to_display, display_date)
+
+    # plt.xticks(range(len(truth)), truth.index)
+
+    plt.savefig(save_folder)
+    plt.show()
+
+
+def plot_pred_classic(df, train, pred, model_name, savefig = "") :
+
+    save_folder = "./plot/" + savefig
+    index = len(train)
+
+    plt.figure(figsize=(8, 6), dpi=100)
+
+    plt.plot(range(len(df['date'])), df['close'], color='black')
+
+
+    lower = np.quantile(pred, 0.05, axis=0)
+    upper = np.quantile(pred, 0.95, axis=0)
+    plt.fill_between(range(index, index + len(lower)), lower, upper, alpha=0.3, color='purple')
+
+    medians = [sorted(sublist)[len(sublist)//2] if len(sublist) % 2 != 0 else
+        (sorted(sublist)[len(sublist)//2 - 1] + sorted(sublist)[len(sublist)//2]) / 2
+        for sublist in zip(*pred)]  
+
+    plt.plot(range(index, index + len(pred[0])), medians, label=model_name, color='purple')
+
+
+    # plt.plot(range(index, index + len(pred[0])), pred[0], label=model_name, color='purple')
+    plt.grid()
+    plt.legend(loc='upper left')
+
+    index_to_display = range(0, len(df["date"]), len(df["date"]) // 3)
+    df['date'] = pd.to_datetime(df['date'])
+    df['date'] = df['date'].dt.date
+    display_date = df["date"][index_to_display]
     plt.xticks(index_to_display, display_date)
 
     # plt.xticks(range(len(truth)), truth.index)
